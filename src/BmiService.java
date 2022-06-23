068d7193e78229a8e2c239bd500c36b48379bae7
Vladimir A <104483890+wmwebmas@users.noreply.github.com>
2022-06-23 11:39:15 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public float calculate(float weight, float height) {
-        float index = weight / (height * 2);
+        float index = weight / (height * height);
         return index;
     }
 }
